f38d6f11f56a67c304806ce0b9c6f9ad7281885d
April Webster <awebster@us.ibm.com>
2017-01-11 12:06:10 -0800

    updated en/v2 test to use the new profile in resources/profiles/v2 and added in tests for v2/es and v2/js

--- test/test.js
+++ test/test.js
@@ -11,22 +11,37 @@ function ( data ) {
 'use strict';
 /*eslint no-console: */
 const assert = require('chai').assert;
-const testProfile = require('../examples/resources/profile');
+const v2EnProfile = require('../examples/profiles/v2/en');
+const v2EsProfile = require('../examples/profiles/v2/es');
 //const v3TestProfile = require('../examples/resources/v3_profile');
 //const TextSummary  = require('../lib/main');
 const TextSummary = require('../lib/index');
 
 //const v3summary = textSummary.getSummary(v3TestProfile);
 //console.log(v3summary);
+//const textSummary = new TextSummary({ locale : 'es', 'version' : 'v2' });
+//const spanishSummary = textSummary.getSummary(testProfiles/);
 
 describe('Text Summary Tests', () => {
 
   it('Get default (English) profile summary:', () => {
-    const textSummary = new TextSummary('en');
-    const summary = textSummary.getSummary(testProfile);
-    //const assemble = textSummary.assemble(testProfile);
-    //console.log('TEST: values ' + assemble);
-    assert.equal(summary, 'You are inner-directed, restrained and rational.\nYou are empathetic: you feel what others feel and are compassionate towards them. You are self-controlled: you have control over your desires, which are not particularly intense. And you are calm-seeking: you prefer activities that are quiet, calm, and safe.\nYour choices are driven by a desire for prestige.\nYou consider helping others to guide a large part of what you do: you think it is important to take care of the people around you. You are relatively unconcerned with tradition: you care more about making your own path than following what others have done.');
+    const textSummary = new TextSummary({ locale : 'en', 'version' : 'v2' });
+    const summary = textSummary.getSummary(v2EnProfile);
+    assert.equal(summary, 'You are shrewd, somewhat critical and particular.\nYou are self-controlled: you have control over your desires, which are not particularly intense. You are mild-tempered: it takes a lot to get you angry. And you are assertive: you tend to speak up and take charge of situations, and you are comfortable leading groups.\nYour choices are driven by a desire for organization.\nYou are relatively unconcerned with both taking pleasure in life and achieving success. You prefer activities with a purpose greater than just personal enjoyment. And you make decisions with little regard for how they show off your talents.');
+
+  });
+
+  it('Get default (Spanish) profile summary:', () => {
+    const textSummary = new TextSummary({ locale : 'es', 'version' : 'v2' });
+    const summary = textSummary.getSummary(v2EsProfile);
+    assert.equal(summary, 'Usted es perspicaz, algo crítico y detallista.\nUsted es sereno: controla sus deseos, los cuales no son particularmente intensos. Usted es apacible: es difícil hacerle enojar. Y usted es asertivo: tiende a expresarse y a hacerse cargo de las situaciones, y se encuentra cómodo liderando grupos.\nSus elecciones están determinadas por un deseo de organización.\nUsted es relativamente indiferente con disfrutar de la vida y alcanzar el éxito. Prefiere actividades con un propósito más grande que el sólo deleite personal. Y toma decisiones sin considerar cómo muestran sus talentos.');
+
+  });
+
+  it('Get default (Japanese) profile summary:', () => {
+    const textSummary = new TextSummary({ locale : 'ja', 'version' : 'v2' });
+    const summary = textSummary.getSummary(v2EsProfile);
+    assert.equal(summary, '鋭敏なタイプであり、多少批判的なタイプであり、また独特なタイプです.\n自制心があるタイプです: 自分の欲望をコントロールできますし、強烈な欲望を持ちません. 温和なタイプです: 滅多に怒りません. また、自己主張が強いタイプです: 遠慮なく発言し、その場をリードする傾向があります。また、集団を統率できます.\n組織への帰属を意識して意思決定するタイプです.\n生活を楽しむことと成功することの両方にあまりこだわりません. 単なる個人の楽しみよりも大きな目標を伴う行動を優先します. また自分の才能を誇示することにあまり拘らず意思決定します.');
 
   });
 
